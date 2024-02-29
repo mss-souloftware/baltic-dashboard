7fb1289732c150159002253050618631f824cff5
mss-souloftware <sufyan.souloftware@gmail.com>
2024-02-29 18:09:34 +0500

    sidebar

--- src/components/Dashboard/E-commerce.tsx
+++ src/components/Dashboard/E-commerce.tsx
@@ -11,7 +11,7 @@ import MapOne from "../Maps/MapOne";
 const ECommerce: React.FC = () => {
   return (
     <>
-      <div className="grid grid-cols-1 gap-4 md:grid-cols-2 md:gap-6 xl:grid-cols-4 2xl:gap-7.5">
+      {/* <div className="grid grid-cols-1 gap-4 md:grid-cols-2 md:gap-6 xl:grid-cols-4 2xl:gap-7.5">
         <CardDataStats title="Total views" total="$3.456K" rate="0.43%" levelUp>
           <svg
             className="fill-primary dark:fill-white"
@@ -96,17 +96,17 @@ const ECommerce: React.FC = () => {
             />
           </svg>
         </CardDataStats>
-      </div>
+      </div> */}
 
       <div className="mt-4 grid grid-cols-12 gap-4 md:mt-6 md:gap-6 2xl:mt-7.5 2xl:gap-7.5">
         <ChartOne />
         <ChartTwo />
-        <ChartThree />
-        <MapOne />
-        <div className="col-span-12 xl:col-span-8">
+        {/* <ChartThree /> */}
+        {/* <MapOne /> */}
+        {/* <div className="col-span-12 xl:col-span-8">
           <TableOne />
-        </div>
-        <ChatCard />
+        </div> */}
+        {/* <ChatCard /> */}
       </div>
     </>
   );
